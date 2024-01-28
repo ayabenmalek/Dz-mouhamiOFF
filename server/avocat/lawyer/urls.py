55e7ad5bf1f02101250e37106217c96efecfe140
Mey362 <m_chikhi@estin.dz>
2024-01-28 16:26:54 +0100

    y

--- server/avocat/lawyer/urls.py
+++ server/avocat/lawyer/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from .views import RegisterView, LoginView, AvocatView, LogoutView, AvocatDatesView, AdminView, FilterView, ReviewCreateView
+from .views import RegisterView, LoginView, AvocatView, LogoutView, AvocatDatesView, AdminView, FilterView, ReviewCreateView, RdvView
 
 urlpatterns = [
     path('register', RegisterView.as_view()),
@@ -9,6 +9,9 @@ urlpatterns = [
     path('logout', LogoutView.as_view()),
     path('avocatdates', AvocatDatesView.as_view()),
     path('admin', AdminView.as_view()),
-    path('filter/', FilterView.as_view()),
-    path('review/<int:avocat_id>/', ReviewCreateView.as_view())
+    path('filter', FilterView.as_view()),
+    path('review/<int:avocat_id>/', ReviewCreateView.as_view()),
+    path('rdv/<int:avocat_id>/', RdvView.as_view()),
+
+    path('rdv/prise_rdv/<int:avocat_id>/', RdvView.as_view())
 ]
