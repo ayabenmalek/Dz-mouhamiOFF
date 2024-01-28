7593c7bbcf3a4261a108a3638c4a0aa7435966d0
Mey362 <m_chikhi@estin.dz>
2024-01-28 03:04:05 +0100

    Update urls.py

--- server/avocat/lawyer/urls.py
+++ server/avocat/lawyer/urls.py
@@ -5,9 +5,9 @@ from .views import RegisterView, LoginView, AvocatView, LogoutView, AvocatDatesV
 urlpatterns = [
     path('register', RegisterView.as_view()),
     path('login', LoginView.as_view()),
-    path('avocat', AvocatView.as_view()),
+    path('avocat/', AvocatView.as_view()),
     path('logout', LogoutView.as_view()),
     path('avocatdates', AvocatDatesView.as_view()),
-    path('admin/<int:avocat_id>', AdminView.as_view()),
+    path('admin', AdminView.as_view()),
     path('filter/', FilterView.as_view())
 ]
